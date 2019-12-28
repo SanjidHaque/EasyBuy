502613fb99d08779b4fbae660dab80f09ab74031
SanjidHaque <sanjidulhaque@gmail.com>
2019-12-28 23:14:48 +0600

    Bottom sheet added again.

--- app/src/main/java/org/tensorflow/lite/examples/detection/CartActivity.java
+++ app/src/main/java/org/tensorflow/lite/examples/detection/CartActivity.java
@@ -13,30 +13,10 @@ import android.view.View;
 import android.widget.Button;
 
 public class CartActivity extends AppCompatActivity {
-
-    public Button shopButton;
-
-
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cart);
-
-        shopButton = findViewById(R.id.shopButton);
-        shopButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                try {
-                    Intent intent = new Intent(CartActivity.this, CameraActivity.class);
-                    startActivity(intent);
-                } catch (Exception e) {
-                    e.printStackTrace();
-                }
-
-            }
-        });
-
     }
 
 }
